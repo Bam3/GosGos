677facaf038f671395dd318f2db9e1cbc6145924
mzgajner <mato@zgajner.com>
2023-08-26 13:21:16 +0200

    Update all npm dependencies and fix a few broken things

--- seeds/clean.js
+++ seeds/clean.js
@@ -1,11 +1,11 @@
 const Expense = require('../models/expense')
 
 module.exports.cleanFaildJobs = async () => {
-    Expense.deleteMany({ description: '1' }, function (err, result) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(result)
-        }
-    })
+    try {
+        const result = await Expense.deleteMany({ description: '1' })
+        console.log(result)
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
