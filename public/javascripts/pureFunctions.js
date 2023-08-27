a3eeb7185a6e54195b1f94dabc97086d1b3dd5f1
mzgajner <mato@zgajner.com>
2023-08-27 12:25:59 +0200

    Simplify grouping of expenses by user/category
    We now only loop through the expenses once to create unique groups and
    to sum together all the expenses.

--- public/javascripts/pureFunctions.js
+++ public/javascripts/pureFunctions.js
@@ -15,60 +15,66 @@ module.exports.calculateSum = (expenses, shared = true) => {
     }
     return sum
 }
-module.exports.roundToTwo = (num) => {
-    return Number(Math.round(num + 'e2') + 'e-2')
-}
-//funkcija prečisti array tako da ostanejo samo unikati,
-//argumenta sta array objektov in properti katerega iščemo.
-module.exports.extractNameAndColor = (
-    arrayOfObjects,
-    propertyName,
-    propertyColor,
-    propertyRoll
-) => {
-    let nameAndColor = {}
-    let allNamesAndColors = []
-    arrayOfObjects.map((object) => {
-        nameAndColor.name = _.get(object, propertyName)
-        nameAndColor.color = _.get(object, propertyColor)
-        nameAndColor.roll = _.get(object, propertyRoll)
-        allNamesAndColors.push(nameAndColor)
-        nameAndColor = {}
-    })
-    //get unique
-    var resArr = []
-    allNamesAndColors.filter(function (item) {
-        var i = resArr.findIndex(
-            (x) =>
-                x.name == item.name &&
-                x.color == item.color &&
-                x.roll == item.roll
-        )
-        if (i <= -1) {
-            resArr.push(item)
-        }
-        return null
-    })
-    return resArr
-}
-module.exports.updateUserOrCategoryClass = (
-    expenses,
-    usersObject,
-    property
-) => {
-    expenses.forEach(function (expense) {
-        usersObject.forEach(function (user) {
-            if (_.get(expense, property) === user.name) {
-                user.payments.push(expense.cost)
+module.exports.roundToTwo = (num) => _.round(num, 2)
+module.exports.groupExpensesByUserOrCategory = (expenses, groupBy) => {
+    // Create an object to track groups by user or category name:
+    //
+    // {
+    //   Miha: {
+    //     name: 'Miha',
+    //     color: '#fafafa',
+    //     numberOfPayments: 12,
+    //     sumOfPayments: 4533,
+    //   },
+    //   ... [other user data here]
+    // }
+    //
+    const groups = {}
+
+    expenses.forEach((expense) => {
+        // If a shared user paid the expense, just ignore it
+        if (groupBy === 'user' && expense.payer.roll === 'shared') return
+
+        // Decide which property to use for name and color
+        const name = groupBy === 'user'
+            ? expense.payer.username
+            : expense.category.parentCategory?.name ?? expense.category.name
+        const color = groupBy === 'user'
+            ? expense.payer.color
+            : expense.category.parentCategory?.color ?? expense.category.color
+
+        // See if we already have this category/user in `groups`
+        const existing = groups[name]
+
+        if (existing) {
+            // This category/user is already tracked, update the existing one
+            existing.numberOfPayments++
+            existing.sumOfPayments += expense.cost
+        } else {
+            // This category/user is new, create it from scratch
+            groups[name] = {
+                name: name,
+                color: color,
+                numberOfPayments: 1,
+                sumOfPayments: expense.cost,
             }
-        })
+        }
     })
-    usersObject.forEach(function (user) {
-        user.numberOfPayments = user.numberOfExpenses
-        user.payments = [user.sumOfExpenses]
-    })
-    // sort by numberOfPayments
-    usersObject.sort((a, b) => b.numberOfPayments - a.numberOfPayments)
+
+    const listOfGroups = Object.values(groups)
+
+    if (groupBy === 'user') {
+        // Sort users by `sumOfPayments` because we want the one who paid
+        // the most to be first
+        listOfGroups.sort((a, b) => b.sumOfPayments - a.sumOfPayments)
+    } else {
+        // Sort categories by `numberOfPayments` because we want to list the
+        // ones with most expenses at the beginning
+        listOfGroups.sort((a, b) => b.numberOfPayments - a.numberOfPayments)
+    }
+
+
+    return listOfGroups
 }
 module.exports.generateCategoryLabel = async (category) => {
     if (category.parentCategory) {
