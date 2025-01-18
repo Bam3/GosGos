d72b98ca23d4a114fcdb6d1150fb9df5d95e38ad
Miha Bastašič <bastasic.miha@gmail.com>
2025-01-18 09:18:52 +0100

    migrate MongoDB

---
+++ migrations-done/20250118081728-add-active-cats.js
@@ -0,0 +1,21 @@
+module.exports = {
+    async up(db, client) {
+        // TODO write your migration here.
+        // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+        // Example:
+        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+        const categories = await db.collection('categories').find({}).toArray()
+        const operations = categories.map((category) => {
+            return db
+                .collection('categories')
+                .updateOne({ _id: category._id }, { $set: { active: true } })
+        })
+        return Promise.all(operations)
+    },
+
+    async down(db, client) {
+        // TODO write the statements to rollback your migration (if possible)
+        // Example:
+        // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    },
+}
